5138a612b67d32dac2d9808f7c50d39ead6236e8
jdy1512 <dyoung1512@gmail.com>
2015-01-13 15:33:37 +0900

    ver0.0.5 jd

---
+++ 2015-01-13 (화)/Member.java
@@ -0,0 +1,17 @@
+public class Member{
+	String id;
+	String password;
+	String name;
+	int mileage;
+	
+	public Member(String id, String password, String name, int mileage){
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.mileage = mileage;
+	}
+	
+	public String getMemberDetails(){
+		return "id : " + id + ", password : " + password + ", name : " + name + ", mileage : " + mileage;
+	}
+}
