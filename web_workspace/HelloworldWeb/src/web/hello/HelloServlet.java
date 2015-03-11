8a73a1298e116f45334e4d621734047b14ff5e33
jdy1512 <dyoung1512@gmail.com>
2015-03-11 17:39:16 +0900

    웹 프로젝트 HelloServlet - 1일차

---
+++ web_workspace/HelloworldWeb/src/web/hello/HelloServlet.java
@@ -0,0 +1,30 @@
+package web.hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
+		String now = sdf.format(date);
+		
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("안녕하세요</br>");
+		out.println("반갑습니다.</br>");
+		out.println("저는 김철수 입니다.</br>");
+		out.println("현재 시간 : " + now);
+	}
+	
+}
